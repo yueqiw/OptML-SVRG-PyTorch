4a4dbb8398efa0da0cf9a3439bae8d47acc00fc2
yueqi.wang.pku@gmail.com <yueqi.wang.pku@gmail.com>
2019-11-23 12:40:39 -0500

    cleaned up and added weight decay

--- svrg.py
+++ svrg.py
@@ -2,73 +2,70 @@ from torch.optim import Optimizer
 import copy
 
 
-#First optimization class for calculating the gradient of the one random selected sample.
 class SVRG_k(Optimizer):
-    r"""Implements stochastic gradient descent (optionally with momentum).
+    r"""Optimization class for calculating the gradient of one iteration.
     Args:
         params (iterable): iterable of parameters to optimize or dicts defining
             parameter groups
         lr (float): learning rate
     """
-    def __init__(self, params, lr):
+    def __init__(self, params, lr, weight_decay=0):
+        print("Using optimizer: SVRG")
         self.u = None
         if lr < 0.0:
             raise ValueError("Invalid learning rate: {}".format(lr))
-
-        defaults = dict(lr=lr)
-        
+        if weight_decay < 0.0:
+            raise ValueError("Invalid weight decay: {}".format(weight_decay))
+        defaults = dict(lr=lr, weight_decay=weight_decay)
         super(SVRG_k, self).__init__(params, defaults)
     
-    def return_param(self):
-          return self.param_groups
-    
-    def set_param(self, new_params):
-          self.param_groups = new_params
+    def get_param_groups(self):
+            return self.param_groups
 
     def set_u(self, new_u):
+        """Set the mean gradient for the current epoch. 
+        """
         if self.u is None:
-          self.u = copy.deepcopy(new_u)
+            self.u = copy.deepcopy(new_u)
         for u_group, new_group in zip(self.u, new_u):  
-          for u, new_u in zip(u_group['params'], new_group['params']):
-            u.grad = new_u.grad.clone()
-    
-    def return_u(self):
-          return self.u
+            for u, new_u in zip(u_group['params'], new_group['params']):
+                u.grad = new_u.grad.clone()
 
     def step(self, params):
         """Performs a single optimization step.
         """
+        for group, new_group, u_group in zip(self.param_groups, params, self.u):
+            weight_decay = group['weight_decay']
 
-        for group, new_group, u_group in zip(self.param_groups, params, self.u):  
             for p, q, u in zip(group['params'], new_group['params'], u_group['params']):
                 if p.grad is None:
                     continue
                 if q.grad is None:
                     continue
-
+                # core SVRG gradient update 
                 new_d = p.grad.data - q.grad.data + u.grad.data
+                if weight_decay != 0:
+                    new_d.add_(weight_decay, p.data)
                 p.data.add_(-group['lr'], new_d)
 
-#optimization class for calculating the mean gradient of all the sample.
-class SVRG_0(Optimizer):
-    r"""Implements stochastic gradient descent (optionally with momentum).
+
+class SVRG_Snapshot(Optimizer):
+    r"""Optimization class for calculating the mean gradient (snapshot) of all samples.
     Args:
         params (iterable): iterable of parameters to optimize or dicts defining
             parameter groups
         lr (float): learning rate
     """
-    def __init__(self, params, lr):
-        if lr < 0.0:
-            raise ValueError("Invalid learning rate: {}".format(lr))
-
-        defaults = dict(lr=lr)
-        
-        super(SVRG_0, self).__init__(params, defaults)
+    def __init__(self, params):
+        defaults = dict()
+        super(SVRG_Snapshot, self).__init__(params, defaults)
       
-    def return_param(self):
-          return self.param_groups
+    def get_param_groups(self):
+            return self.param_groups
     
-    def set_param(self, new_params):
-         for group, new_group in zip(self.param_groups, new_params):  #[1,2,3] [4,5,6] => [(1,4), (2,5)]
+    def set_param_groups(self, new_params):
+        """Copies the parameters from another optimizer. 
+        """
+        for group, new_group in zip(self.param_groups, new_params): 
             for p, q in zip(group['params'], new_group['params']):
                   p.data[:] = q.data[:]
